2c6724b7bea6686807ec02443115c0245233db45
Manuel Casares <manucasaresrojas@gmail.com>
2020-08-21 13:18:05 -0300

    docs agregado

---
+++ docs/precache-manifest.3044abdb062cf55b24d30fea7647d548.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e61c95a628b109b7c46bb6667d29da96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6aee6a62940f1afaee18",
+    "url": "/static/css/main.b41c745e.chunk.css"
+  },
+  {
+    "revision": "341da49108025e1aee60",
+    "url": "/static/js/2.e59cd077.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e59cd077.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6aee6a62940f1afaee18",
+    "url": "/static/js/main.5422a2c3.chunk.js"
+  },
+  {
+    "revision": "c75e9e798accc7134ce9",
+    "url": "/static/js/runtime-main.ee800c48.js"
+  }
+]);
